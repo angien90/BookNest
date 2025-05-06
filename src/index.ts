4571844faf3e5689ee65db47e1ffcbb22b3c0a19
angien90 <angelicanylander@hotmail.com>
2025-05-06 14:27:29 +0200

    Add necessary installations and database

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import 'dotenv/config'
+import express from 'express';
+import cors from 'cors'
+import mongoose from 'mongoose';
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cors({
+  origin: "*",         
+  credentials: true    // Allows cookies sent to this API
+}));        
+
+
+// Routes
+import bookRouter from './routes/books'
+import userRouter from './routes/user'
+app.use('/books', bookRouter)
+app.use('/user', userRouter)
+
+// Connect To DB
+mongoose.connect(process.env.MONGODB_URL || "");
+
+// Start the express server
+const PORT = 3000
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`)
+})
