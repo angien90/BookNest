beb67b7004d80fcda1ec681c668a58a3b9ee61cf
tgvie <166875380+tgvie@users.noreply.github.com>
2025-05-07 20:50:12 +0200

    add user router for patch and delete (unprotected)

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -9,7 +9,12 @@ import { fetchAllBooks } from '../controller/booksController';
 
 const router = express.Router();
 
+// Base endpoints
 router.get('/', fetchAllUsers)
 router.get('/:id', fetchUserById)
+
+// Protected endpoints (NOT YET)
+router.patch('/:id', updateUser);
+router.delete('/:id', deleteUser);
 
 export default router;
