e97d361f5fc2c2c94e0d046b34f76e6236078328
tgvie <166875380+tgvie@users.noreply.github.com>
2025-05-07 20:26:45 +0200

    update user router

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,8 +1,15 @@
 import express from 'express';
+import {
+  fetchAllUsers,
+  fetchUserById,
+  updateUser,
+  deleteUser
+} from '../controller/usersController';
+import { fetchAllBooks } from '../controller/booksController';
+
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.send('Funkar!');
-});
+router.get('/', fetchAllUsers)
+router.get('/:id', fetchUserById)
 
 export default router;
