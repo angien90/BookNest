80dd54d3bb77ccb057976a105e9678a3879ccc74
angien90 <angelicanylander@hotmail.com>
2025-05-09 15:21:43 +0200

    Add variables and button component

---
+++ book-client/src/components/Button.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { computed } from "vue";
+import { RouterLink } from "vue-router";
+
+const props = defineProps({
+  buttonText: String,
+  to: String,
+});
+
+// Beräkna om vi ska använda <RouterLink> eller <button>
+const isLink = computed(() => !!props.to);
+</script>
+
+<template>
+  <component
+    :is="isLink ? RouterLink : 'button'"
+    :to="isLink ? to : null"
+    class="button"
+  >
+    {{ buttonText }}
+  </component>
+</template>
+
+<style lang="scss" scoped>
+.button {
+  @include button;
+}
+</style>
