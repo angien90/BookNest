7e0d3eb5c721d957f9dc20ec5d9e8e4508375505
angien90 <angelicanylander@hotmail.com>
2025-05-09 16:42:08 +0200

    Add structure for vue

---
+++ book-client/src/components/Footer.vue
@@ -0,0 +1,14 @@
+<script setup>
+</script>
+
+<template>
+  <footer>
+
+        <h2>Vår footer</h2>
+
+  </footer>
+</template>
+
+<style scoped lang="scss">
+
+</style>
---
+++ book-client/src/components/StartPage.vue
@@ -0,0 +1,16 @@
+<script setup>
+</script>
+
+<template>
+  <footer>
+
+        <h2>Vår startpage</h2>
+
+      Här kommer det ligga en klickbar bild på en bok som tar mig till componenten BookInformation.vue
+
+  </footer>
+</template>
+
+<style scoped lang="scss">
+
+</style>
