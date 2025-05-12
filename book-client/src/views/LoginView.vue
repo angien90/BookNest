2679086a50389ab80ae0cec27feac349a448db8e
tgvie <166875380+tgvie@users.noreply.github.com>
2025-05-12 16:40:42 +0200

    add base for LoginView

--- book-client/src/views/LoginView.vue
+++ book-client/src/views/LoginView.vue
@@ -1,9 +1,19 @@
 <script setup>
+import Header from '@/components/Header.vue';
+import Footer from '@/components/Footer.vue';
+//import LoginForm from '@/components/LoginForm.vue';
 </script>
 
 <template>
-    
+    <main>
+        <Header />
+        <LoginForm />
+        <Footer />        
+    </main>
 </template>
 
-<style>
+<style lang='scss' scoped>
+main {
+    background-color: $creamwhite;
+}
 </style>
