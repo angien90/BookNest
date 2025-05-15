b701d370523737324a9470eba173419a63350e49
M-Lenvik <lenvik.marie@gmail.com>
2025-05-15 18:52:40 +0200

    after git pull

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ app.use(compression());
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: "http://localhost:5173",         
+  origin: "http://localhost:5173",
   credentials: true    // Allows cookies sent to this API
 }));        
 
