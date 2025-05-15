2f4c235b5f29a56e5207652ae647cdffe646b290
tgvie <166875380+tgvie@users.noreply.github.com>
2025-05-15 06:39:48 +0200

    fetch all users to adminpanel and its styling

---
+++ book-client/src/views/AdminUserView.vue
@@ -0,0 +1,19 @@
+<script setup>
+import Header from '@/components/Header.vue';
+import AdminPanelUsers from '@/components/AdminPanelUsers.vue';
+import Footer from '@/components/Footer.vue';
+</script>
+
+<template>
+  <main>
+    <Header />
+    <AdminPanelUsers />
+    <Footer />
+  </main>
+</template>
+
+<style lang="scss" scoped>
+main {
+  background-color: $creamwhite;
+}
+</style>
