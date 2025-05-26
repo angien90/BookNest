91d3ab9ea0606e1d4b24679f3532a3254edbe3e0
angien90 <angelicanylander@hotmail.com>
2025-05-26 15:06:16 +0200

    fix links Vercel

--- book-client/src/components/Review.vue
+++ book-client/src/components/Review.vue
@@ -260,7 +260,7 @@ const cancelDelete = () => {
         <Header>
             <template #logo>
                 <img 
-                :src="'/public/images/' + book.image" 
+                :src="'/BookNest/public/images/' + book.image" 
                 :alt="book.title"
                 class="book_image"
                 />
